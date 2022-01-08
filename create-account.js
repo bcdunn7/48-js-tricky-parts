708a43c8a91332f2cdfba7bd3f24191b2502ab4a
Bryce <bryce.cc.dunn@gmail.com>
2022-01-07 22:30:17 -0600

    Formatting)

--- create-account.js
+++ create-account.js
@@ -1,11 +1,11 @@
-function createAccount(startPin, amount=0) {
+function createAccount(startPin, amount = 0) {
     let pin = startPin;
     let balance = amount;
 
     return {
         checkBalance: (pinEntry) => {
             if (!(pinEntry === pin)) return "Invalid PIN.";
-            
+
             return `$${balance}`;
         },
         deposit: (pinEntry, depositAmt) => {
@@ -16,7 +16,7 @@ function createAccount(startPin, amount=0) {
         },
         withdraw: (pinEntry, withdrawAmt) => {
             if (!(pinEntry === pin)) return "Invalid PIN.";
-            if (balance-withdrawAmt < 0) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+            if (balance - withdrawAmt < 0) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
 
             balance -= withdrawAmt;
             return `Succesfully withdrew $${withdrawAmt}. Current balance: $${balance}.`
